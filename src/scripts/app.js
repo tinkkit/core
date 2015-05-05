108e6238fea6199cdeb8edd6020826fcd25d58a1
Jasper Van Proeyen <trianglejuice@mac.com>
2015-05-05 17:44:58 +0200

    Trying some things out like adding Angular and some components without adding them to the build

---
+++ src/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tinkApp
+ * @description
+ * # tinkApp
+ *
+ * Main module of the application.
+ */
+ angular
+ .module('tinkApp', [
+   'ngAnimate',
+   'ngCookies',
+   'ngResource',
+   'ngRoute',
+   'ngMessages',
+   'ngSanitize',
+   'ngTouch',
+   'tink.tinkApi',
+   'tink.navigation'
+
+   ])
+ .config(function ($routeProvider) { /*, $locationProvider */
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+
+  // $locationProvider.html5Mode(true);
+  // $locationProvider.hashPrefix('!');
+});
